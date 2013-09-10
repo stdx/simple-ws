63a5c452ac899c39d8f9cfbee92dd8f0faeaa126
Stefan Dahms <std@stdx.de>
2013-09-10 21:23:44 +0200

    minor changes.

--- src/main/java/org/stdx/ws/simple/impl/SayHelloServiceImpl.java
+++ src/main/java/org/stdx/ws/simple/impl/SayHelloServiceImpl.java
@@ -4,15 +4,19 @@ import org.stdx.webservice.helloworld.SayHello;
 import org.stdx.webservice.helloworld.SayHelloResponse;
 import org.stdx.ws.simple.HelloWorldServiceSkeletonInterface;
 
-
 /**
  * The Class SayHelloServiceImpl.
  */
 public class SayHelloServiceImpl implements HelloWorldServiceSkeletonInterface {
 
-	
-	/* (non-Javadoc)
-	 * @see com.foo.myservice.HelloWorldServiceSkeletonInterface#sayHello(com.moodykettle.webservice.helloworld.SayHello)
+	private static final String WORLD = "world";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.stdx.ws.simple.HelloWorldServiceSkeletonInterface#sayHello(org.stdx
+	 * .webservice.helloworld.SayHello)
 	 */
 	public SayHelloResponse sayHello(SayHello sayHello) {
 
@@ -22,19 +26,19 @@ public class SayHelloServiceImpl implements HelloWorldServiceSkeletonInterface {
 		return response;
 	}
 
-	
 	/**
 	 * Say hello.
-	 *
-	 * @param name the name
+	 * 
+	 * @param name
+	 *            the name
 	 * @return the string
 	 */
 	private String sayHello(String name) {
-		String to = "world";
-		if (name != null || name.trim().length() != 0) {
+		String to = WORLD;
+		if (name != null && name.trim().length() != 0) {
 			to = name;
 		}
-		return to;
+		return "hello " + to;
 	}
 
 }
